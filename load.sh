63615dcdffef5c00c672dec2e58f445b17d32f6e
Anton Mukhin <6122dante@gmail.com>
2021-09-10 14:03:08 +0300

    Load.sh: Add entry for second LED

--- load.sh
+++ load.sh
@@ -4,7 +4,10 @@ device="LED_CTRL"
 major=200
 
 rm -f /dev/${device} c $major 0
-
 mknod /dev/${device} c $major 0
 
+rm -f /dev/${device}1 c $major 1
+mknod /dev/${device}1 c $major 1
+
 chmod 666 /dev/${device}
+chmod 666 /dev/${device}1
